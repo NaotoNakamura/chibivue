dfa159e95110570125d40c93e166f83be4522457
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2023-09-17 21:29:00 +0900

    textContent以外のDOM操作にも対応できるようにDOM操作用関数を別途作成
    【変更前】
    
    - createAppの引数に渡されたオブジェクトのrenderメソッドを直接実行
    - renderメソッドから返された値をtextContentに代入
    
    【変更後】
    
    - nodeOpsにDOM操作するメソッドを定義
    - createRendererでは、引数に渡されたnodeOpsのメソッドをラップしたrenderメソッドをオブジェクトとして返す
        - renderメソッドには挿入したいテキストとnodeを渡すことで、nodeOpsのメソッドを実行

--- packages/index.ts
+++ packages/index.ts
@@ -6,12 +6,44 @@ export type App = {
   mount: (selector: string) => void;
 };
 
+export interface RendererNode {
+  [key: string]: any;
+}
+
+export interface RendererOptions<HostNode = RendererNode> {
+  setElementText(node: HostNode, text: string): void;
+}
+
+export interface RendererElement extends RendererNode {}
+
+export type RootRenderFunction<HostElement = RendererElement> = (
+  message: string,
+  container: HostElement
+) => void;
+
+export function createRenderer(options: RendererOptions) {
+  const { setElementText: hostSetElementText } = options;
+
+  const render: RootRenderFunction = (message, container) => {
+    hostSetElementText(container, message);
+  };
+
+  return { render };
+}
+
+export const nodeOps: RendererOptions<Node> = {
+  setElementText(node, text) {
+    node.textContent = text;
+  },
+};
+
 export const createApp = (options: Options): App => {
   return {
     mount: (selector: string) => {
       const container = document.querySelector(selector);
       if (!container) return;
-      container.textContent = options.render();
+      const { render } = createRenderer(nodeOps);
+      render(options.render(), container);
     },
   };
 };
